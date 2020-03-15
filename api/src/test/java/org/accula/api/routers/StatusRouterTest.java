4c2514499f99e41764ea9e5818c56fb4d40d83fb
Anton Lamtev <antonlamtev@gmail.com>
2020-03-15 20:43:27 +0300

    remove unused import

--- api/src/test/java/org/accula/api/routers/StatusRouterTest.java
+++ api/src/test/java/org/accula/api/routers/StatusRouterTest.java
@@ -1,6 +1,5 @@
 package org.accula.api.routers;
 
-import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
