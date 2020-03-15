1f784613472ca7aed0624395c0d35a0fcceecedc
Anton Lamtev <antonlamtev@gmail.com>
2020-03-15 21:13:37 +0300

    Use Component annotation instead of manual bean declaration

--- api/src/main/java/org/accula/api/handlers/GreetingHandler.java
+++ api/src/main/java/org/accula/api/handlers/GreetingHandler.java
@@ -1,6 +1,7 @@
 package org.accula.api.handlers;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
@@ -9,6 +10,7 @@ import static java.util.function.Predicate.not;
 import static org.springframework.web.reactive.function.server.ServerResponse.*;
 import static org.springframework.web.reactive.function.server.ServerResponse.badRequest;
 
+@Component
 public final class GreetingHandler {
     private static final String GREETING = "ACCULA is greeting you, ";
     private static final String ERROR = "Missing required query param \"name\"";
