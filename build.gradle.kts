216bf1b9a7a11d3cae432e2e098c2d92c031f6f3
Anton Lamtev <antonlamtev@gmail.com>
2020-03-29 13:54:06 +0300

    JaCoCo code coverage and codecov integration (#24) (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,61 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+
+plugins {
+    java
+    jacoco
+}
+
+jacoco {
+    toolVersion = "0.8.5"
+}
+
 allprojects {
     group = "org.accula"
+}
+
+configure(subprojects.filterNot(project(":web")::equals)) {
+    apply(plugin = "java")
+    apply(plugin = "jacoco")
 
     repositories {
         jcenter()
+    }
+
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_14
+    }
+
+    tasks {
+        jacocoTestReport {
+            executionData(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
+
+            subprojects.forEach {
+                sourceSets(it.sourceSets["main"])
+            }
+
+            reports {
+                sourceDirectories.from(files(sourceSets["main"].allSource.srcDirs))
+                classDirectories.from(files(sourceSets["main"].output))
+
+                csv.isEnabled = false
+
+                listOf(xml, html).forEach { report ->
+                    report.isEnabled = true
+                    report.destination = file("$buildDir/reports/jacoco/coverage.${report.name}")
+                }
+            }
+        }
+
+        test {
+            useJUnitPlatform()
+
+            testLogging {
+                events(PASSED, SKIPPED, FAILED)
+            }
+
+            finalizedBy(jacocoTestReport)
+        }
     }
 }
