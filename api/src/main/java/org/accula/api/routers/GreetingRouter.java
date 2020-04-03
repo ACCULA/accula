fb014c30f8e3748aae1f8175451773f3de17116e
Anton Lamtev <antonlamtev@gmail.com>
2020-04-03 23:34:31 +0300

    add some smells to code

--- api/src/main/java/org/accula/api/routers/GreetingRouter.java
+++ api/src/main/java/org/accula/api/routers/GreetingRouter.java
@@ -17,6 +17,135 @@ public final class GreetingRouter {
     @Bean
     @NotNull
     public RouterFunction<ServerResponse> greetingRoute() {
+        final var bool1 = hashCode() % 2 == 0;
+        final var bool2 = hashCode() % 3 == 0;
+        if (bool1) {
+            if (bool2) {
+                if (bool1 && bool2) {
+                    if (bool1 || bool 2) {
+                        if (bool1 ^ bool2) {
+                            if (true) {
+                                System.out.println("");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
+        new Object();
         return RouterFunctions
                 .route()
                 .GET("/greet", handler::greet)
