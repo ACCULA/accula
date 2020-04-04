7a019e571e2716f7f133e1a63a49f300e03aea00
Anton Lamtev <antonlamtev@gmail.com>
2020-04-04 22:33:34 +0300

    Automate code quality checks for Java code #26 (#27)
    codeclimate
    * pmd
    * sonar
    * checkstyle

--- api/src/main/java/org/accula/api/AcculaApiApplication.java
+++ api/src/main/java/org/accula/api/AcculaApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcculaApiApplication {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(AcculaApiApplication.class, args);
     }
 }
--- api/src/main/java/org/accula/api/routers/GreetingRouter.java
+++ api/src/main/java/org/accula/api/routers/GreetingRouter.java
@@ -2,7 +2,6 @@ package org.accula.api.routers;
 
 import lombok.RequiredArgsConstructor;
 import org.accula.api.handlers.GreetingHandler;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -15,7 +14,6 @@ public final class GreetingRouter {
     private final GreetingHandler handler;
 
     @Bean
-    @NotNull
     public RouterFunction<ServerResponse> greetingRoute() {
         return RouterFunctions
                 .route()
