cb1d6afd6be41bfc22bb0572edb48aa7f6caea0e
vaddya <vadik.dyachkov@gmail.com>
2020-05-15 00:21:05 +0300

    fix test

--- api/src/test/java/org/accula/api/routers/StatusRouterTest.java
+++ api/src/test/java/org/accula/api/routers/StatusRouterTest.java
@@ -1,15 +1,15 @@
 package org.accula.api.routers;
 
-import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-@SpringBootTest()
-@AutoConfigureEmbeddedDatabase(beanName = "dataSource")
+@WebFluxTest
+@ContextConfiguration(classes = {StatusRouter.class})
 public class StatusRouterTest {
     private static final String STATUS = "{\"status\":\"ONLINE\"}";
 
