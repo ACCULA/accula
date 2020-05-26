a5b1e6e616c61e83b1bb0dd9b202d4e7a268b640
vaddya <vadik.dyachkov@gmail.com>
2020-05-26 20:50:11 +0300

    fix

--- api/src/main/java/org/accula/api/code/CodeLoaderImpl.java
+++ api/src/main/java/org/accula/api/code/CodeLoaderImpl.java
@@ -26,7 +26,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.StringJoiner;
 
 /**
