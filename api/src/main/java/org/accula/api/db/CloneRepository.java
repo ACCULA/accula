b4bb7a22b5b8a5b47c41296c36a00c897b21bfb2
vaddya <vadik.dyachkov@gmail.com>
2020-05-28 02:31:12 +0300

    fix

--- api/src/main/java/org/accula/api/db/CloneRepository.java
+++ api/src/main/java/org/accula/api/db/CloneRepository.java
@@ -2,7 +2,6 @@ package org.accula.api.db;
 
 import org.accula.api.db.model.Clone;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author Vadim Dyachkov
--- api/src/main/java/org/accula/api/db/RefreshTokenRepository.java
+++ api/src/main/java/org/accula/api/db/RefreshTokenRepository.java
@@ -4,7 +4,6 @@ import org.accula.api.db.model.RefreshToken;
 import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
