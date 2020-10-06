e6f910dde2a64eab125a182e6d3f0e7a85464c24
Egor Shakhmin <lapter57@gmail.com>
2020-10-06 04:17:59 +0300

    Updated a Projects page

--- web/src/views/Projects/Projects.tsx
+++ web/src/views/Projects/Projects.tsx
@@ -1,99 +1,132 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useSnackbar } from 'notistack'
 import { bindActionCreators } from 'redux'
 import { connect, ConnectedProps } from 'react-redux'
-import { Button, Clearfix, Col, Grid, Row } from 'react-bootstrap'
 import { Helmet } from 'react-helmet'
-
-import { Breadcrumbs } from 'components/Breadcrumbs'
-import { Loader } from 'components/Loader'
+import { AddBoxOutlined } from '@material-ui/icons'
 import { AppDispatch, AppState } from 'store'
-import {
-  createProjectAction,
-  getProjectsAction,
-  resetCreateProject
-} from 'store/projects/actions'
-import { CreateProjectModal } from './CreateProjectModal'
-import { ProjectPanel } from './ProjectPanel'
+import { getProjectsAction } from 'store/projects/actions'
+import Button from '@material-ui/core/Button'
+import BreadCrumbs from 'components/BreadCrumbs'
+import Tabs from 'components/Tabs'
+import Table from 'components/Table'
+import { HeadCell } from 'components/Table/components/TableHeader/TableHeader'
+import { IProject } from 'types'
+import layersImg from 'images/layers.svg'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import { Avatar, IconButton, TableCell } from '@material-ui/core'
+import { StyledTableRow } from 'components/Table/styles'
+import { useStyles } from './styles'
 
-const mapStateToProps = (state: AppState) => ({
-  isFetching: state.projects.projects.isFetching || !state.projects.projects.value,
-  user: state.users.user.value,
-  projects: state.projects.projects.value,
-  creationState: state.projects.createProject
-})
+type ProjectsProps = PropsFromRedux
 
-const mapDispatchToProps = (dispatch: AppDispatch) => ({
-  getProjects: bindActionCreators(getProjectsAction, dispatch),
-  createProject: bindActionCreators(createProjectAction, dispatch),
-  resetCreationState: () => dispatch(resetCreateProject())
-})
+const headCells: HeadCell[] = [
+  { id: 'name', numeric: false, disablePadding: false, label: 'Name' },
+  { id: 'open pr', numeric: true, disablePadding: false, label: 'Open pr' },
+  { id: 'link', numeric: true, disablePadding: false, label: '' }
+]
 
-const connector = connect(mapStateToProps, mapDispatchToProps)
-type ProjectsProps = ConnectedProps<typeof connector>
-
-const Projects = ({
-  user, //
-  isFetching,
-  projects,
-  creationState,
-  getProjects,
-  createProject,
-  resetCreationState
-}: ProjectsProps) => {
-  const [isShowModal, setShowModal] = useState(false)
+const Projects = ({ projects, getProjects }: ProjectsProps) => {
+  const { enqueueSnackbar } = useSnackbar()
+  const classes = useStyles()
 
   useEffect(() => {
-    getProjects()
+    getProjects(message => enqueueSnackbar(message, { variant: 'error' }))
+    // eslint-disable-next-line
   }, [getProjects])
 
-  useEffect(() => {
-    if (!creationState[0] && creationState[1] === null) {
-      setShowModal(false)
-    }
-  }, [creationState, setShowModal])
+  if (!projects) {
+    return <></>
+  }
 
-  return isFetching ? (
-    <Loader />
-  ) : (
-    <div className="content">
+  if (projects.length === 0) {
+    return (
+      <div className={classes.emptyContent}>
+        <img className={classes.layersImg} src={layersImg} alt="Projects" />
+        <span className={classes.projectsText}>Projects</span>
+        <Button className={classes.addProjectBtn} variant="contained" color="secondary">
+          Add project
+        </Button>
+      </div>
+    )
+  }
+  const el = <AddBoxOutlined />
+  return (
+    <div>
       <Helmet>
         <title>Projects - ACCULA</title>
       </Helmet>
-      <Grid fluid className="tight">
-        <Clearfix>
-          <div className="pull-right">
-            {user && (
-              <Button bsStyle="info" style={{ marginTop: -5 }} onClick={() => setShowModal(true)}>
-                <i className="fa fa-plus" /> Add project
-              </Button>
-            )}
-          </div>
-          <Breadcrumbs breadcrumbs={[{ text: 'Projects' }]} />
-        </Clearfix>
-        <Row>
-          {projects.map(project => (
-            <Col key={project.id} xs={12} sm={6} md={6} lg={4}>
-              <ProjectPanel {...project} />
-            </Col>
-          ))}
-        </Row>
-      </Grid>
-      <CreateProjectModal
-        show={isShowModal}
-        error={creationState[1]}
-        isCreating={creationState[0]}
-        resetError={resetCreationState}
-        onClose={() => {
-          resetCreationState()
-          setShowModal(false)
-        }}
-        onSubmit={url => {
-          resetCreationState()
-          createProject(url)
-        }}
-      />
+      <div className={classes.breadcrumbs}>
+        <BreadCrumbs breadcrumbs={[{ text: 'Projects' }]} />
+      </div>
+      <div className={classes.tabs}>
+        <Tabs />
+      </div>
+      <Table<IProject>
+        headCells={headCells}
+        toolBarTitle=""
+        toolBarButtons={[
+          {
+            toolTip: 'Add project',
+            iconButton: el,
+            onClick: () => console.log('Add project')
+          }
+        ]}
+      >
+        {projects.map(project => (
+          <StyledTableRow
+            hover
+            onClick={() => console.log('Click on', project.repoName)}
+            tabIndex={-1}
+            key={project.id}
+          >
+            <TableCell align="left">
+              <div className={classes.repoInfo}>
+                <Avatar
+                  className={classes.repoAvatarImg}
+                  src={project.repoOwnerAvatar}
+                  alt={project.repoOwner}
+                />
+                <div className={classes.repoFullName}>
+                  <span
+                    className={classes.cellText}
+                  >{`${project.repoOwner}/${project.repoName}`}</span>
+                  <span className={classes.repoDescription}>{project.repoDescription || ''}</span>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell align="right">
+              <span className={classes.cellText}>{project.repoOpenPullCount}</span>
+            </TableCell>
+            <TableCell align="right">
+              <a
+                href={project.repoUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classes.repoUrlImg}
+              >
+                <IconButton color="default" aria-label="Project Url">
+                  <GitHubIcon />
+                </IconButton>
+              </a>
+            </TableCell>
+          </StyledTableRow>
+        ))}
+      </Table>
     </div>
   )
 }
+
+const mapStateToProps = (state: AppState) => ({
+  projects: state.projects.projects.value
+})
+
+const mapDispatchToProps = (dispatch: AppDispatch) => ({
+  getProjects: bindActionCreators(getProjectsAction, dispatch)
+})
+
+const connector = connect(mapStateToProps, mapDispatchToProps)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
 
 export default connector(Projects)
---
+++ web/src/views/Projects/styles.ts
@@ -0,0 +1,61 @@
+import { makeStyles, Theme } from '@material-ui/core'
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  emptyContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: 'auto'
+  },
+  layersImg: {
+    width: '152px',
+    height: '152px'
+  },
+  projectsText: {
+    fontSize: '38px',
+    fontWeight: 500,
+    marginBottom: '28px'
+  },
+  addProjectBtn: {
+    boxShadow: 'none',
+    '&:hover': {
+      boxShadow: 'none'
+    }
+  },
+  breadcrumbs: {
+    marginBottom: '24px'
+  },
+  tabs: {
+    marginBottom: '63px'
+  },
+  repoUrlImg: {
+    width: '33px',
+    height: '33px'
+  },
+  cellText: {
+    fontSize: '18px',
+    fontWeight: 500
+  },
+  repoInfo: {
+    display: 'flex',
+    alignContent: 'center'
+  },
+  repoFullName: {
+    marginLeft: '10px',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  repoDescription: {
+    color: theme.palette.type === 'light' ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)'
+  },
+  repoAvatarImg: {
+    width: '52px',
+    height: '52px'
+  }
+}))
