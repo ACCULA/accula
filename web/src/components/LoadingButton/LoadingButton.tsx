3c2e8a506022d6a6b1cbd04707baa5e52f08ef27
Egor Shakhmin <lapter57@gmail.com>
2020-10-07 18:45:48 +0300

    Updated LoadingButton

---
+++ web/src/components/LoadingButton/LoadingButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { CircularProgress, Button } from '@material-ui/core'
+
+import { useStyles } from './styles'
+
+interface LoadingButtonProps {
+  text: string
+  submitting: boolean
+  onClick?: () => void
+}
+
+const LoadingButton = ({ text, submitting, onClick }: LoadingButtonProps) => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.buttonWrapper}>
+      <Button
+        type="submit"
+        variant="contained"
+        color="secondary"
+        disabled={submitting}
+        onClick={onClick}
+      >
+        {text}
+      </Button>
+      {submitting && (
+        <CircularProgress color="secondary" size={24} className={classes.buttonProgress} />
+      )}
+    </div>
+  )
+}
+
+export default LoadingButton
