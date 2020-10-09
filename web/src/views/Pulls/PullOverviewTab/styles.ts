db3ea951d282214940ecf644e631e3eba38f9949
Egor Shakhmin <lapter57@gmail.com>
2020-10-09 23:14:31 +0300

    Updated PullOverviewTab component

---
+++ web/src/views/Pulls/PullOverviewTab/styles.ts
@@ -0,0 +1,96 @@
+import { makeStyles, Theme } from '@material-ui/core'
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  pullOverview: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '20px 0',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column'
+    }
+  },
+  authorAvatar: {
+    width: 192,
+    height: 192,
+    boxShadow: '0 4px 60px rgba(0,0,0,.3)'
+  },
+  authorView: {
+    marginRight: 24,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      margin: 0
+    }
+  },
+  githubButton: {
+    width: 24,
+    height: 24
+  },
+  authorLoginField: {
+    display: 'flex',
+    alignItems: 'center',
+    color: 'inherit',
+    textDecoration: 'none',
+    marginTop: 10
+  },
+  authorLogin: {
+    fontWeight: 700,
+    marginLeft: 8
+  },
+  pullInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    marginLeft: 20,
+    [theme.breakpoints.down('sm')]: {
+      margin: 0
+    }
+  },
+  pullTitle: {
+    marginBottom: 8,
+    marginTop: 0,
+    fontSize: 48,
+    [theme.breakpoints.down('md')]: {
+      fontSize: 38
+    },
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+      marginTop: 10
+    }
+  },
+  prImage: {
+    width: 16,
+    height: 16,
+    minWidth: 16,
+    minHeight: 16,
+    marginRight: 6,
+    fill: theme.palette.type === 'light' ? 'rgba(0, 0, 0, 0.7)' : '#fff'
+  },
+  pullInfoField: {
+    display: 'flex',
+    alignItems: 'end',
+    marginBottom: 4,
+    color: theme.palette.type === 'light' ? 'rgba(0, 0, 0, 0.7)' : '#fff',
+    fontSize: '0.9rem'
+  },
+  pullStatus: {
+    marginLeft: 6,
+    width: 14,
+    height: 14
+  },
+  dateField: {
+    marginLeft: 4
+  },
+  githubLink: {
+    color: '#2178a3',
+    background: '#e3f2f9'
+  },
+  pullRequestsBlock: {
+    marginTop: 40
+  },
+  titleOfBlock: {
+    fontSize: 24,
+    fontWeight: 400
+  }
+}))
