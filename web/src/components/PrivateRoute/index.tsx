fd4c3d2948e341ae2b85987c61ec403bcc473b62
Egor Shakhmin <lapter57@gmail.com>
2020-10-11 14:53:26 +0300

    Show error notification when authentication is required

--- web/src/components/PrivateRoute/index.tsx
+++ web/src/components/PrivateRoute/index.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import { Route, RouteProps } from 'react-router-dom'
-import { AuthWall } from 'components/AuthWall'
+import { Redirect, Route, RouteProps } from 'react-router-dom'
+import { getNotifier } from 'App'
+import { useSnackbar } from 'notistack'
 
 interface PrivateRouteProps extends RouteProps {
   auth: boolean
 }
 
 export const PrivateRoute = ({ component: Component, auth, ...rest }: PrivateRouteProps) => {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        auth ? ( //
-          <Component {...props} />
-        ) : (
-          <AuthWall />
-        )
-      }
-    />
-  )
+  const snackbarContext = useSnackbar()
+  console.log(auth)
+  if (!auth) {
+    getNotifier('error', snackbarContext)('Authentication required')
+    return (
+      <Redirect
+        to={{
+          pathname: '/'
+        }}
+      />
+    )
+  }
+
+  return <Route {...rest} render={props => <Component {...props} />} />
 }
