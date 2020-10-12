6f719acdfc5fc97c164aecdedb7ebe15663d3454
Egor Shakhmin <lapter57@gmail.com>
2020-10-12 04:41:35 +0300

    Wrapped PullLabel in a ToolTip

--- web/src/components/PullLabel/PullLabel.tsx
+++ web/src/components/PullLabel/PullLabel.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import clsx from 'clsx'
+import { Tooltip } from '@material-ui/core'
 import { useStyles } from './styles'
 
 interface PullLabel {
@@ -10,7 +11,11 @@ interface PullLabel {
 const PullLabel = ({ text, className }: PullLabel) => {
   const classes = useStyles()
 
-  return <code className={clsx(className, classes.label)}>{text}</code>
+  return (
+    <Tooltip placement="top" title={text}>
+      <code className={clsx(className, classes.label)}>{text}</code>
+    </Tooltip>
+  )
 }
 
 export default PullLabel
