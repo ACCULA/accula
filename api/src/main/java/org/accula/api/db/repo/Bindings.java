6118c04b909cfda721dddcde3e5bf244a4126792
Anton Lamtev <lamtev@users.noreply.github.com>
2021-04-08 01:21:09 +0300

    default access

--- api/src/main/java/org/accula/api/db/repo/Bindings.java
+++ api/src/main/java/org/accula/api/db/repo/Bindings.java
@@ -3,11 +3,11 @@ package org.accula.api.db.repo;
 /**
  * @author Anton Lamtev
  */
-public final class Bindings {
+final class Bindings {
     private Bindings() {
     }
 
-    public static Object[] of(final Object... row) {
+    static Object[] of(final Object... row) {
         return row;
     }
 }
