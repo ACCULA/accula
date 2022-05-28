475d4a519d08ad18dc82f1476252fb756552d0e8
Anton Lamtev <lamtev@users.noreply.github.com>
2022-05-28 20:45:25 +0300

    Upgrade SB to 2.7.0, r2dbc to 0.9.1 (#305)

---
+++ api/src/main/java/org/accula/api/db/repo/StatementUtils.java
@@ -0,0 +1,27 @@
+package org.accula.api.db.repo;
+
+import io.r2dbc.spi.Statement;
+
+import java.util.function.BiConsumer;
+
+/**
+ * @author Anton Lamtev
+ */
+final class StatementUtils {
+    private StatementUtils() {
+    }
+
+    static <S extends Statement, T> Statement bindIterable(
+            final Iterable<T> iterable,
+            final S statement,
+            final BiConsumer<T, S> bind) {
+        final var iterator = iterable.iterator();
+        while (iterator.hasNext()) {
+            bind.accept(iterator.next(), statement);
+            if (iterator.hasNext()) {
+                statement.add();
+            }
+        }
+        return statement;
+    }
+}
